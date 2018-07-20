2b2b443cf9729dc53107256298e7580cf6091a10
Aleksei Kuzmin <alkuzmin@microsoft.com>
2018-07-20 18:08:18 +0200

    The Great Blink mv for source files, part 2.
    https://chromium-review.googlesource.com/1001153

--- atom/renderer/api/atom_api_web_frame.h
+++ atom/renderer/api/atom_api_web_frame.h
@@ -12,7 +12,7 @@
 #include "atom/renderer/guest_view_container.h"
 #include "native_mate/handle.h"
 #include "native_mate/wrappable.h"
-#include "third_party/WebKit/public/platform/WebCache.h"
+#include "third_party/blink/public/platform/web_cache.h"
 
 namespace blink {
 class WebLocalFrame;
